cf2091b465674e944b5785febc0afa9e3c60129b
Luke Ferrier <lferrier901@gmail.com>
2026-02-26 22:03:55 +0100

    fix: removed unused imports

--- app/builtins/echo.ts
+++ app/builtins/echo.ts
@@ -1,7 +1,5 @@
 import * as IOE from 'fp-ts/lib/IOEither';
 import type { Command, CommandArgs, EvalResult } from '../types/Command';
-import type { CommandError } from '../types/Error';
-import type { CommandResult } from '../types/Result';
 
 export const echo: Command<CommandArgs> = {
   eval: (args: CommandArgs): EvalResult =>
