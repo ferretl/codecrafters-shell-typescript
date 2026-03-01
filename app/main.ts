cde8e76809b119c91eb91863e6df855646ed3c75
Luke Ferrier <lferrier901@gmail.com>
2026-03-01 17:05:39 +0100

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -53,6 +53,7 @@ export const runExecutable = (
       () =>
         spawnSync(`${dir}/${name}`, [...args], {
           argv0: name,
+          encoding: 'utf-8',
           stdio: ['pipe', 'pipe', 'inherit']
         }).stdout,
       (): { message: string } => ({ message: `${name}: command failed` })
