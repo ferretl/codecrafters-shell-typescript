d9095459c728be3694f53cb0f629640fcb3040f4
Luke Ferrier <lferrier901@gmail.com>
2026-03-01 11:18:47 +0100

    feat: added handling for no arugments for type function

--- app/builtins/type.ts
+++ app/builtins/type.ts
@@ -1,29 +1,32 @@
 import * as IOE from 'fp-ts/lib/IOEither';
 import * as O from 'fp-ts/Option';
+import * as RA from 'fp-ts/ReadonlyArray';
 import type { Command, CommandArgs, IOEvalResult } from '../types/Command';
 import { findBuiltin, findExecutable } from '.';
 import { pipe } from 'fp-ts/lib/function';
 
-
 export const type: Command<CommandArgs> = {
-  eval: (args: CommandArgs): IOEvalResult => {
-    const input = args[0];
-
-    const text = pipe(
-      pipe(
-        findBuiltin(input),
-        O.map(() => `${input} is a shell builtin`)
-      ),
-      O.alt(() => pipe(
-        findExecutable(input),
-        O.map((filePath) => `${input} is ${filePath}/${input}`)
-      )),
-      O.getOrElse(() => `${input}: not found`)
-    );
-
-    return IOE.right({
-      _tag: 'Output',
-      text
-    });
-  }
+  eval: (args: CommandArgs): IOEvalResult =>
+    pipe(
+      RA.head(args),
+      O.fold(
+        () => IOE.left({ message: 'No arguments given!' }),
+        (name) => {
+          const text = pipe(
+            pipe(
+              findBuiltin(name),
+              O.map(() => `${name} is a shell builtin`)
+            ),
+            O.alt(() =>
+              pipe(
+                findExecutable(name),
+                O.map((filePath) => `${name} is ${filePath}/${name}`)
+              )
+            ),
+            O.getOrElse(() => `${name}: not found`)
+          );
+          return IOE.right({ _tag: 'Output', text });
+        }
+      )
+    )
 };
