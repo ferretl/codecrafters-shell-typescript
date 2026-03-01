17c1d1d5ed5cf276b34ff0aa51bba9a1a3404687
Luke Ferrier <lferrier901@gmail.com>
2026-03-01 10:37:36 +0100

    codecrafters submit [skip ci]

--- app/builtins/type.ts
+++ app/builtins/type.ts
@@ -1,21 +1,47 @@
 import * as IOE from 'fp-ts/lib/IOEither';
 import * as O from 'fp-ts/Option';
+import * as A from 'fp-ts/Array';
 import type { Command, CommandArgs, IOEvalResult } from '../types/Command';
 import { builtins } from '.';
 import { pipe } from 'fp-ts/lib/function';
+import path from 'path';
+import fs from 'fs';
+
+const findBuiltin = (input: string): O.Option<string> =>
+  pipe(
+    O.fromNullable(builtins[input]),
+    O.map(() => `${input} is a shell builtin`)
+  );
+
+const findExecutable = (input: string): O.Option<string> =>
+  pipe(
+    O.fromNullable(process.env.PATH),
+    O.map((pathContents) => pathContents.split(path.delimiter)),
+    O.chain(
+      A.findFirst((filePath) =>
+        O.isSome(
+          O.tryCatch(() => {
+            fs.accessSync(`${filePath}/${input}`, fs.constants.X_OK);
+          })
+        )
+      )
+    ),
+    O.map((filePath) => `${input} is ${filePath}/${input}`)
+  );
 
 export const type: Command<CommandArgs> = {
   eval: (args: CommandArgs): IOEvalResult => {
-    const command = args[0];
+    const input = args[0];
+
+    const text = pipe(
+      findBuiltin(input),
+      O.alt(() => findExecutable(input)),
+      O.getOrElse(() => `${input}: not found`)
+    );
+
     return IOE.right({
       _tag: 'Output',
-      text: pipe(
-        O.fromNullable(builtins[command]),
-        O.match(
-          () => `${command}: not found`,
-          () => `${command} is a shell builtin`
-        )
-      )
+      text
     });
   }
 };
