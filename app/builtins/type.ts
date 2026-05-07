3c6b53fb136907a8c2b066a6576e2653edae94c9
Luke Ferrier <lferrier901@gmail.com>
2026-05-07 15:44:50 +1000

    refactor: flattened pipes, used consistent naming conventions

--- app/builtins/type.ts
+++ app/builtins/type.ts
@@ -15,23 +15,21 @@ export const type: Command = {
   eval: (args): IOEvalResult =>
     pipe(
       RA.head(args),
-      O.fold(
+      O.match(
         () => IOE.left({ message: 'No arguments given!' }),
         (name) => {
           const text = pipe(
-            pipe(
-              findBuiltin(name),
-              O.map(() => `${name} is a shell builtin`)
-            ),
+            findBuiltin(name),
+            O.map(() => `${name} is a shell builtin`),
             O.alt(() =>
               pipe(
                 findExecutable(name),
                 O.map((filePath) => `${name} is ${filePath}/${name}`)
               )
             ),
-            O.alt(() => O.some(`${name} not found`))
+            O.getOrElse(() => `${name} not found`)
           );
-          return IOE.right(output(text));
+          return IOE.right(pipe(text, O.some, output));
         }
       )
     )
