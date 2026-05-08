eaf70fff4d361ce8a033d94c8bf70225e6297e76
Luke Ferrier <lferrier901@gmail.com>
2026-05-08 14:25:45 +1000

    refactor: refactored to use expectOutput function

--- app/tests/builtinTests/pwd.test.ts
+++ app/tests/builtinTests/pwd.test.ts
@@ -1,49 +1,20 @@
-import { afterEach, expect, test } from "bun:test";
+import { afterEach, test } from "bun:test";
 import * as O from "fp-ts/Option";
 import * as pwd from "../../builtins/pwd";
-import { ResultTag } from "../../types";
+import { expectOutput } from "../helpers";
 
 const orignalCwd = process.cwd();
 afterEach(() => process.chdir(orignalCwd));
 
 test("pwd should return the current working directory", () => {
-	const result = pwd.pwd([])();
-	const expected = O.some(process.cwd());
-	expect(result._tag).toBe("Right");
-	if (result._tag === "Right") {
-		expect(result.right._tag).toBe(ResultTag.Output);
-		if (result.right._tag === "Output") {
-			const text = result.right.text;
-			expect(text).toEqual(expected);
-		}
-	}
+	expectOutput(pwd.pwd([])(), O.some(process.cwd()));
 });
 
 test("pwd should ignore any arguments", () => {
-	const result = pwd.pwd(["unexpected", "arguments"])();
-	const expected = O.some(process.cwd());
-	expect(result._tag).toBe("Right");
-	if (result._tag === "Right") {
-		expect(result.right._tag).toBe(ResultTag.Output);
-		if (result.right._tag === "Output") {
-			const text = result.right.text;
-			expect(text).toEqual(expected);
-		}
-	}
+	expectOutput(pwd.pwd(["unexpected", "arguments"])(), O.some(process.cwd()));
 });
 
 test("if we change the directory, pwd should reflect that change", () => {
-	const original = process.cwd();
 	process.chdir("..");
-	const result = pwd.pwd([])();
-	const expected = O.some(process.cwd());
-	expect(result._tag).toBe("Right");
-	if (result._tag === "Right") {
-		expect(result.right._tag).toBe(ResultTag.Output);
-		if (result.right._tag === "Output") {
-			const text = result.right.text;
-			expect(text).toEqual(expected);
-		}
-	}
-	process.chdir(original); // Change back to the original directory after the test
+	expectOutput(pwd.pwd([])(), O.some(process.cwd()));
 });
