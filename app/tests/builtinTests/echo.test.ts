537fbc78715c341b87cd316f30794c0d3112075a
Luke Ferrier <lferrier901@gmail.com>
2026-05-08 13:52:55 +1000

    feat: added tests for builtin functions

---
+++ app/tests/builtinTests/echo.test.ts
@@ -0,0 +1,12 @@
+import { test } from 'bun:test';
+import * as O from 'fp-ts/Option';
+import * as echo from '../../builtins/echo';
+import { expectOutput } from '../helpers';
+
+test('echo should return the concatenated arguments as output', () => {
+  expectOutput(echo.echo(['Hello', 'world!'])(), O.some('Hello world!'));
+});
+
+test('echo with no arguments should return an empty string', () => {
+  expectOutput(echo.echo([])(), O.some(''));
+});
---
+++ app/tests/builtinTests/pwd.test.ts
@@ -0,0 +1,49 @@
+import { afterEach, expect, test } from 'bun:test';
+import * as pwd from '../../builtins/pwd';
+import { ResultTag, type CommandArgs } from '../../types';
+import * as O from 'fp-ts/Option';
+
+const orignalCwd = process.cwd();
+afterEach(() => process.chdir(orignalCwd));
+
+test('pwd should return the current working directory', () => {
+  const result = pwd.pwd([])();
+  const expected = O.some(process.cwd());
+  expect(result._tag).toBe('Right');
+  if (result._tag === 'Right') {
+    expect(result.right._tag).toBe(ResultTag.Output);
+    if (result.right._tag === 'Output') {
+      const text = result.right.text;
+      expect(text).toEqual(expected);
+    }
+  }
+});
+
+test('pwd should ignore any arguments', () => {
+  const result = pwd.pwd(['unexpected', 'arguments'])();
+  const expected = O.some(process.cwd());
+  expect(result._tag).toBe('Right');
+  if (result._tag === 'Right') {
+    expect(result.right._tag).toBe(ResultTag.Output);
+    if (result.right._tag === 'Output') {
+      const text = result.right.text;
+      expect(text).toEqual(expected);
+    }
+  }
+});
+
+test('if we change the directory, pwd should reflect that change', () => {
+  const original = process.cwd();
+  process.chdir('..');
+  const result = pwd.pwd([])();
+  const expected = O.some(process.cwd());
+  expect(result._tag).toBe('Right');
+  if (result._tag === 'Right') {
+    expect(result.right._tag).toBe(ResultTag.Output);
+    if (result.right._tag === 'Output') {
+      const text = result.right.text;
+      expect(text).toEqual(expected);
+    }
+  }
+  process.chdir(original); // Change back to the original directory after the test
+});
