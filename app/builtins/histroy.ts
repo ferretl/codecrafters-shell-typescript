cb6938d5625d9b4da0b25ac834a56914ba236970
Luke Ferrier <lferrier901@gmail.com>
2026-05-25 18:47:07 +1000

    fix: preserved history number, fixed spelling mistake, simplified convertToNumber

--- app/builtins/histroy.ts
+++ app/builtins/histroy.ts
@@ -3,6 +3,7 @@ import * as IO from "fp-ts/IO";
 import type { HistoryRef } from "../histroy";
 import * as RA from "fp-ts/ReadonlyArray";
 import * as O from "fp-ts/Option";
+import * as S from "fp-ts/lib/string";
 import {
 	builtinCommand,
 	type Command,
@@ -15,32 +16,34 @@ const format = (
 	entries: ReadonlyArray<string>,
 	historyLimit: O.Option<number>,
 ) => {
-	const limitedEntries = O.isSome(historyLimit)
-		? RA.takeRight(historyLimit.value)(entries)
-		: entries;
+	const formattedEntries = entries.map(
+		(line, i) => `${String(i + 1).padStart(5, " ")}  ${line}\n`,
+	);
 
-	return limitedEntries
-		.map((line, i) => `${String(i + 1).padStart(5, " ")}  ${line}\n`)
-		.join("");
+	return pipe(
+		O.isSome(historyLimit)
+			? RA.takeRight(historyLimit.value)(formattedEntries)
+			: formattedEntries,
+		RA.intercalate(S.Monoid)(""),
+	);
 };
 
 const convertToNumber = (input: O.Option<string>): O.Option<number> =>
 	pipe(
 		input,
-		O.map(parseInt),
-		O.fromPredicate((parsedInput) => !Number.isNaN(parsedInput)),
-		O.flatten,
+		O.map((input) => parseInt(input, 10)),
+		O.filter((parsedInput) => !Number.isNaN(parsedInput)),
 	);
 
 export const makeHistory =
 	(ref: HistoryRef): Command =>
 	(args) => {
-		const histroyLimit = pipe(args, RA.head, convertToNumber);
+		const historyLimit = pipe(args, RA.head, convertToNumber);
 		return pipe(
 			ref.read,
 			IO.map((entries) =>
 				builtinCommand(
-					fromString(format(entries, histroyLimit)),
+					fromString(format(entries, historyLimit)),
 					empty(),
 					normal,
 				),
