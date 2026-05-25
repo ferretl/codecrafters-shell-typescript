499626a6ac28a9ff7779a2febef39744f851eeca
Luke Ferrier <lferrier901@gmail.com>
2026-05-25 18:41:06 +1000

    feat: added limiting feature for history builtin

--- app/builtins/histroy.ts
+++ app/builtins/histroy.ts
@@ -1,6 +1,8 @@
 import { pipe } from "fp-ts/function";
 import * as IO from "fp-ts/IO";
 import type { HistoryRef } from "../histroy";
+import * as RA from "fp-ts/ReadonlyArray";
+import * as O from "fp-ts/Option";
 import {
 	builtinCommand,
 	type Command,
@@ -9,17 +11,39 @@ import {
 	normal,
 } from "../types";
 
-const format = (entries: ReadonlyArray<string>): string =>
-	entries
+const format = (
+	entries: ReadonlyArray<string>,
+	historyLimit: O.Option<number>,
+) => {
+	const limitedEntries = O.isSome(historyLimit)
+		? RA.takeRight(historyLimit.value)(entries)
+		: entries;
+
+	return limitedEntries
 		.map((line, i) => `${String(i + 1).padStart(5, " ")}  ${line}\n`)
 		.join("");
+};
+
+const convertToNumber = (input: O.Option<string>): O.Option<number> =>
+	pipe(
+		input,
+		O.map(parseInt),
+		O.fromPredicate((parsedInput) => !Number.isNaN(parsedInput)),
+		O.flatten,
+	);
 
 export const makeHistory =
 	(ref: HistoryRef): Command =>
-	() =>
-		pipe(
+	(args) => {
+		const histroyLimit = pipe(args, RA.head, convertToNumber);
+		return pipe(
 			ref.read,
 			IO.map((entries) =>
-				builtinCommand(fromString(format(entries)), empty(), normal),
+				builtinCommand(
+					fromString(format(entries, histroyLimit)),
+					empty(),
+					normal,
+				),
 			),
 		);
+	};
