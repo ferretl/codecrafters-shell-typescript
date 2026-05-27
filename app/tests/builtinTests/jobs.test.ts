3df7dcde97410afa2af3f9c567c0d5a3b8c658f8
Luke Ferrier <lferrier901@gmail.com>
2026-05-27 17:34:16 +1000

    fix: removed unneeded helper

--- app/tests/builtinTests/jobs.test.ts
+++ app/tests/builtinTests/jobs.test.ts
@@ -1,11 +1,11 @@
-import test from "node:test";
+import { expect, test } from "bun:test";
 import { pipe } from "fp-ts/lib/function";
+import * as O from "fp-ts/Option";
 import { findBuiltin, makeBuiltins } from "../../builtins";
 import { jobs } from "../../builtins/jobs";
 import { makeHistoryRef } from "../../histroy";
-import { expectSome } from "../helpers";
 
 test("jobs has the builtin type", () => {
-	const commandFinder = pipe(makeHistoryRef(), makeBuiltins, findBuiltin);
-	expectSome(commandFinder("jobs"), jobs);
+	const builtinFinder = pipe(makeHistoryRef(), makeBuiltins, findBuiltin);
+	expect(builtinFinder("jobs")).toBe(O.some(jobs));
 });
