66364aa429cd5a8ed5dbd8118951ae1e684d3942
Luke Ferrier <lferrier901@gmail.com>
2026-06-10 18:42:32 +1000

    feat: simplified import

--- app/tests/builtinTests/echo.test.ts
+++ app/tests/builtinTests/echo.test.ts
@@ -1,12 +1,12 @@
 import { test } from "bun:test";
-import * as echo from "../../builtins/echo";
+import { echo } from "../../builtins/echo";
 import { empty } from "../../commmandTypes";
 import { expectStdout } from "../helpers";
 
 test("echo should return the concatenated arguments as output", async () => {
-	await expectStdout(echo.echo(["Hello", "world!"], empty()), "Hello world!\n");
+	await expectStdout(echo(["Hello", "world!"], empty()), "Hello world!\n");
 });
 
 test("echo with no arguments should return just a newline", async () => {
-	await expectStdout(echo.echo([], empty()), "\n");
+	await expectStdout(echo([], empty()), "\n");
 });
